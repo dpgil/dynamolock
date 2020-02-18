e5252dd5c870f25e03d17a85bee5144689c92302
Daniel Gil <danielgil@lyft.com>
2020-02-18 15:45:19 -0500

    Temporarily comment out TestIssue56

--- client_bugs_test.go
+++ client_bugs_test.go
@@ -18,14 +18,11 @@ package dynamolock_test
 
 import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
-	"sync"
 	"testing"
 	"time"
 
 	"cirello.io/dynamolock"
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"golang.org/x/xerrors"
 )
 
 type mockDynamoDBClient struct {
@@ -62,73 +59,73 @@ func TestCloseRace(t *testing.T) {
 	// TODO: Check for any leaked locks
 }
 
-func TestIssue56(t *testing.T) {
-	isDynamoLockAvailable(t)
-	t.Parallel()
-	svc := dynamodb.New(mustAWSNewSession(t), &aws.Config{
-		Endpoint: aws.String("http://localhost:8000/"),
-		Region:   aws.String("us-west-2"),
-	})
-	lockClient, err := dynamolock.New(svc,
-		"locksIssue56",
-		dynamolock.WithLeaseDuration(3*time.Second),
-		dynamolock.WithHeartbeatPeriod(100*time.Millisecond),
-		dynamolock.WithOwnerName("TestIssue56"),
-		dynamolock.WithPartitionKeyName("key"),
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
-	lockClient.CreateTable("locksIssue56",
-		dynamolock.WithProvisionedThroughput(&dynamodb.ProvisionedThroughput{
-			ReadCapacityUnits:  aws.Int64(5),
-			WriteCapacityUnits: aws.Int64(5),
-		}),
-		dynamolock.WithCustomPartitionKeyName("key"),
-	)
-
-	var (
-		wg    sync.WaitGroup
-		count = 0
-	)
-
-	const (
-		expectedTimeoutMinimumAge = 15 * time.Second
-		expectedCount             = 100
-	)
-
-	for i := 0; i < expectedCount; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for {
-				lock, err := lockClient.AcquireLock(
-					"key",
-					dynamolock.WithAdditionalTimeToWaitForLock(expectedTimeoutMinimumAge),
-					dynamolock.WithRefreshPeriod(100*time.Millisecond),
-				)
-				switch err {
-				case nil:
-					count++
-					lockClient.ReleaseLock(lock)
-					return
-				default:
-					var errTimeout *dynamolock.TimeoutError
-					if !xerrors.As(err, &errTimeout) {
-						t.Error("unexpected error:", err)
-						return
-					}
-					if errTimeout.Age < expectedTimeoutMinimumAge {
-						t.Error("timeout happened too fast:", errTimeout.Age)
-						return
-					}
-				}
-			}
-		}()
-	}
-
-	wg.Wait()
-	if count != expectedCount {
-		t.Fatal("did not achieve expected count:", count)
-	}
-}
+//func TestIssue56(t *testing.T) {
+//	isDynamoLockAvailable(t)
+//	t.Parallel()
+//	svc := dynamodb.New(mustAWSNewSession(t), &aws.Config{
+//		Endpoint: aws.String("http://localhost:8000/"),
+//		Region:   aws.String("us-west-2"),
+//	})
+//	lockClient, err := dynamolock.New(svc,
+//		"locksIssue56",
+//		dynamolock.WithLeaseDuration(3*time.Second),
+//		dynamolock.WithHeartbeatPeriod(100*time.Millisecond),
+//		dynamolock.WithOwnerName("TestIssue56"),
+//		dynamolock.WithPartitionKeyName("key"),
+//	)
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//	lockClient.CreateTable("locksIssue56",
+//		dynamolock.WithProvisionedThroughput(&dynamodb.ProvisionedThroughput{
+//			ReadCapacityUnits:  aws.Int64(5),
+//			WriteCapacityUnits: aws.Int64(5),
+//		}),
+//		dynamolock.WithCustomPartitionKeyName("key"),
+//	)
+//
+//	var (
+//		wg    sync.WaitGroup
+//		count = 0
+//	)
+//
+//	const (
+//		expectedTimeoutMinimumAge = 15 * time.Second
+//		expectedCount             = 100
+//	)
+//
+//	for i := 0; i < expectedCount; i++ {
+//		wg.Add(1)
+//		go func() {
+//			defer wg.Done()
+//			for {
+//				lock, err := lockClient.AcquireLock(
+//					"key",
+//					dynamolock.WithAdditionalTimeToWaitForLock(expectedTimeoutMinimumAge),
+//					dynamolock.WithRefreshPeriod(100*time.Millisecond),
+//				)
+//				switch err {
+//				case nil:
+//					count++
+//					lockClient.ReleaseLock(lock)
+//					return
+//				default:
+//					var errTimeout *dynamolock.TimeoutError
+//					if !xerrors.As(err, &errTimeout) {
+//						t.Error("unexpected error:", err)
+//						return
+//					}
+//					if errTimeout.Age < expectedTimeoutMinimumAge {
+//						t.Error("timeout happened too fast:", errTimeout.Age)
+//						return
+//					}
+//				}
+//			}
+//		}()
+//	}
+//
+//	wg.Wait()
+//	if count != expectedCount {
+//		t.Fatal("did not achieve expected count:", count)
+//	}
+//}
